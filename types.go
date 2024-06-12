d06676d2f1666e98ddaae0c681ee5b66a0e1e9e5
Alejandro Ruiz <alejandro.ruiz@suse.com>
2024-06-12 12:26:10 +0200

    fix: use independent deadline for sending error
    See https://github.com/rancher/remotedialer/pull/79

--- types.go
+++ types.go
@@ -7,4 +7,6 @@ const (
 	PingWriteInterval = 5 * time.Second
 	MaxRead           = 8192
 	HandshakeTimeOut  = 10 * time.Second
+	// SendErrorTimeout sets the maximum duration for sending an error message to close a single connection
+	SendErrorTimeout = 5 * time.Second
 )
